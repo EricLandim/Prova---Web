1b34dd3867918871ef91fc45b5c19af25e99229d
EricLandim <81322865+EricLandim@users.noreply.github.com>
2022-01-14 21:14:04 -0300

    Add files via upload

---
+++ Domain/Helpers/EUnitOfPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace api_rest.Domain.Helpers
+{
+    public enum EUnitOfMovie : byte
+    {
+        [Description("acao")]
+        acao = 1,
+
+        [Description("comédia")]
+        comédia = 2,
+
+        [Description("drama")]
+        drama = 3,
+
+    }
+
+}
